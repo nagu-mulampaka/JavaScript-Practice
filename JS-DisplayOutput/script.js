ca366c593723dc0bc22571e0912fb5bae2d96c2e
Nagu Mulampaka <nagumulampaka@gmail.com>
2025-12-30 21:48:49 +0530

    Create script.js

---
+++ JS-DisplayOutput/script.js
@@ -0,0 +1,20 @@
+// script.js file
+// Program demonstrating how to display output on a website in multiple ways
+
+// console.log() — Used to view output in the browser console.
+console.log("Hello Everyone, My Name is Nagu Mulampaka");
+
+// alert() — Popup Alert Box
+alert("This is alert Box");
+
+// confirm() — To ask for user confirmation (Yes/No or OK/Cancel).
+console.log(confirm("Do you want to continue?"));
+
+// prompt() — Input Box: To get user input via a popup box.
+document.writeln(prompt("Enter your name:"));
+
+// console.warn() — Used to display warning messages to the browser console.
+console.warn("This is a warning message.");
+
+// console.error() — Used to display error messages in the browser console
+console.error("This is an error message.");
