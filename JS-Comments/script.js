6cd0f27aaaab9278df962474577da73f4e7f3a92
Nagu Mulampaka <nagumulampaka@gmail.com>
2025-12-30 21:45:28 +0530

    Add JavaScript comments demonstration program

---
+++ JS-Comments/script.js
@@ -0,0 +1,12 @@
+console.log("Program demonstrating comments in JavaScript");
+console.log("Welcome to the JavaScript course");
+
+// Single-line comments
+// console.log("Hello everyone, my name is Nagu Mulampaka");
+// console.log("I am from Vizag, MVP Colony, Visakhapatnam, Andhra Pradesh");
+
+/* Multi-line comments */
+/* 
+console.log("I completed my Master's in Computer Applications (MCA)");
+console.log("From Dr. Lankapalli Bullayya PG College");
+*/
