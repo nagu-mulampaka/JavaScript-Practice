8c4592167177169e1a33271047651505ef4c26ea
Nagu Mulampaka <nagumulampaka@gmail.com>
2026-01-05 21:21:56 +0530

    Add examples of JavaScript operators

---
+++ JS-Operators/script.js
@@ -0,0 +1,64 @@
+// Arithmetic Operators Example
+let a = 10;
+let b = 20;
+
+// Addition
+console.log("Addition Results is: ", a + b);
+
+// Subtraction
+console.log("Subtraction Results is:", a - b);
+
+// Multiplication
+console.log("Multiplication Results is:", a * b);
+
+// Division
+console.log("Division Results is:", a / b);
+
+// Modulus
+console.log("Modulus Results is: ", a % b);
+
+// Unary Operators Example
+let c = 30;
+
+// Increment
+console.log("Pre-Increment (++c) :", ++c); // 31
+console.log("Post-Increment (c++) :", c++); // 31
+console.log("C value after Post-Increment value is: ", c); //32
+
+// Decrement
+console.log("Pre-Decrement (--c) : ", --c); // 31
+console.log("Post-Decrement (c--) : ", c--); // 31
+console.log("C value after Post-Decrement value is: ", c); // 30
+
+// Unary plus
+let d = 40;
+console.log("Unary plus (+d): ", +d);
+console.log("Unary plus (-d): ", -d); // -40
+
+let username = "Mulampaka Nagu";
+console.log(typeof d); // number
+console.log(typeof username); // string
+
+// Assignment Operators Example
+let x = 10;
+console.log("Initial x Value is: ", x);
+
+// Add and assign
+x += 5; // x = x + 5
+console.log("After += 5:", x); // 15
+
+// Subtract and assign
+x -= 3; // x = x - 3
+console.log("After -= 3:", x); // 12
+
+// Multiply and assign
+x *= 2; // x = x * 2
+console.log("After *= 2:", x); // 24
+
+// Divide and assign
+x /= 4; // x = x / 4
+console.log("After /= 4:", x); // 6
+
+// Modulus and assign
+x %= 4; // x = x % 4
+console.log("After %= 4:", x); // 2
